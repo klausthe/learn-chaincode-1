b421d024f0847585b2444a9c25f009d22f969266
Percival Lucena <plucena@gmail.com>
2016-12-14 14:09:06 -0200

    Update chaincode_finished.go

--- finished/chaincode_finished.go
+++ finished/chaincode_finished.go
@@ -69,7 +69,7 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 
 	// Handle different functions
 	if function == "read" { //read a variable
-		var msg = "Estou de ferias. Volto logo!"		
+		var msg = "testando o servidor local do risc"		
 		return []byte(msg), nil 		
 		//return t.read(stub, args)
 	}
